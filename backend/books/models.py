86c56e5f154ae93c0002c4088ccaa74d108d50b7
Victoria Vanessa Parra Franco <victoriaparrafranco@gmail.com>
2026-09-14 19:34:41 -0400

    Añadir migración inicial para el modelo y configurar aplicaciones en settings.py

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from books.infrastructure.models import BookORM  # noqa: F401
 
 # Create your models here.
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,9 +27,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'books',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
